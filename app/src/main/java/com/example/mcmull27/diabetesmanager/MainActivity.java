ddacf26d7a6a29c5662c78274c24650f7d8197da
Michael McMullen <mcmull27@uwm.edu>
2018-07-07 15:33:27 -0500

    Added spinner to activity form, removed average heart rate text field and renamed activity title field to more closely match project description, and changed colors again

--- app/src/main/java/com/example/mcmull27/diabetesmanager/MainActivity.java
+++ app/src/main/java/com/example/mcmull27/diabetesmanager/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.mcmull27.diabetesmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.activity_type_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.activity_type_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
+
 }
