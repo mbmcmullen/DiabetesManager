d4868ba8d22e6a975cb225623b2e723ba3a396f6
crsather <crsather@uwm.edu>
2018-07-17 16:18:34 -0500

    deleteById and selectAll methods implemented; added default constructor; added Locale.US to simpleDateFormat to fix a warning

--- app/src/main/java/com/example/mcmull27/diabetesmanager/DatabaseManager.java
+++ app/src/main/java/com/example/mcmull27/diabetesmanager/DatabaseManager.java
@@ -1,5 +1,6 @@
 package com.example.mcmull27.diabetesmanager;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,6 +9,8 @@ import android.util.Log;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class DatabaseManager extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "diabetesManagerDB";
@@ -18,6 +21,10 @@ public class DatabaseManager extends SQLiteOpenHelper {
     private static final String DESCRIPTION = "description";
     private static final String TIMESTAMP = "timestamp";
     private static final String AMOUNT = "amount";
+
+    public DatabaseManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -69,11 +76,48 @@ public class DatabaseManager extends SQLiteOpenHelper {
                         cursor.getString(1),
                         cursor.getString(2),
                         Double.parseDouble((cursor.getString(3))),
-                        new SimpleDateFormat("dd/MM/yyyy").parse(cursor.getString(4)));
+                        new SimpleDateFormat("dd/MM/yyyy",Locale.US).parse(cursor.getString(4)));
         }catch(ParseException e){
             Log.e("DatabaseManager", "parse exception caught in selectById. Activity database id:"+id);
         }
+        cursor.close();
+        db.close();
         return ret;
     }
+    public void deleteByID(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String sqlDelete = "delete from " + TABLE_ACTIVITIES;
+        sqlDelete += " where " + ID + " = " + id;
 
+        db.execSQL(sqlDelete);
+        db.close();
+    }
+
+    public ArrayList<Act> selectAll(){
+        ArrayList<Act> activities = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String sqlQuery = "select * from " + TABLE_ACTIVITIES;
+        Cursor cursor = db.rawQuery(sqlQuery, null);
+
+
+        try {
+            while (cursor.moveToNext()) {
+                Act currentActivity = new Act(
+                        Integer.parseInt(cursor.getString(0)),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        Double.parseDouble((cursor.getString(3))),
+                        new SimpleDateFormat("dd/MM/yyyy", Locale.US).parse(cursor.getString(4)));
+
+                activities.add(currentActivity);
+            }
+            db.close();
+            cursor.close();
+        }catch(ParseException e)   {
+            Log.e("DatabaseManager", "parse exception caught in selectAll. Activity database id: " + Integer.parseInt(cursor.getString(0)));
+        }
+
+        return activities;
+    }
 }
