39f859de41ade6bb95b460fa63f16257f4ba96ca
crsather <crsather@uwm.edu>
2018-07-18 13:24:26 -0500

    added java files for each layout to inflate themselves when run.

--- app/src/main/java/com/example/mcmull27/diabetesmanager/MainActivity.java
+++ app/src/main/java/com/example/mcmull27/diabetesmanager/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.mcmull27.diabetesmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+
 
 
 
@@ -14,15 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Spinner spinner = (Spinner) findViewById(R.id.activity_type_spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.activity_type_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
     }
 
 
