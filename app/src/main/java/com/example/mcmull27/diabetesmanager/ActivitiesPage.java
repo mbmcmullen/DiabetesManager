e71be20a7b2f0afc307c1e2fa736f806994e3249
crsather <crsather@uwm.edu>
2018-07-18 13:48:05 -0500

    added java files for each layout to inflate themselves when run.

---
+++ app/src/main/java/com/example/mcmull27/diabetesmanager/ActivitiesPage.java
@@ -0,0 +1,21 @@
+package com.example.mcmull27.diabetesmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+public class ActivitiesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activities_page);
+
+
+
+    }
+
+
+}
